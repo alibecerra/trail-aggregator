d086135aa47d50f6a150fc76e2b0c06b248156f9
Aj <53246298+alibecerra@users.noreply.github.com>
2025-12-26 14:53:10 -0800

    started with simple structure of layout, basic data scraper was made for quotes

---
+++ app/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+
+namespace WebScraper
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Add(
+                "User-Agent",
+                "Mozilla/5.0 (compatible; MyCSharpBot/1.0)"
+            );
+
+            string url = "http://quotes.toscrape.com/";
+
+            Console.WriteLine("Starting scraper...");
+            Console.WriteLine($"Fetching: {url}\n");
+
+            try
+            {
+                string html = await client.GetStringAsync(url);
+
+                HtmlDocument doc = new HtmlDocument();
+                doc.LoadHtml(html);
+
+                var quotes = doc.DocumentNode.SelectNodes("//div[@class='quote']");
+
+                if (quotes != null)
+                {
+                    Console.WriteLine($"Found {quotes.Count} quotes!\n");
+
+                    int counter = 1;
+                    foreach (var quote in quotes)
+                    {
+                        string text = quote.SelectSingleNode(".//span[@class='text']")?.InnerText;
+                        string author = quote
+                            .SelectSingleNode(".//small[@class='author']")
+                            ?.InnerText;
+
+                        Console.WriteLine($"Quote #{counter}:");
+                        Console.WriteLine($"  Text: {text}");
+                        Console.WriteLine($"  Author: {author}");
+                        Console.WriteLine();
+
+                        counter++;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No quotes found!");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error occurred: {ex.Message}");
+            }
+
+            Console.WriteLine("Scraping complete!");
+        }
+    }
+}
